02d3dd980b231ac307a15d5431ecceab7d255d7a
0xsterben <0xsterben@tutamail.com>
2024-05-20 17:21:30 +0200

    J'ai comencé la route users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+require('../models/connection')
+const User = require('../models/users')
+const { checkBody } = require("../models/check")
+
+
+
+
+
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
 module.exports = router;
