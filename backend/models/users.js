d3bd92eea9ddc2f67b7fdd4cefe66cb679678d51
0xsterben <0xsterben@tutamail.com>
2024-05-20 16:23:30 +0200

    J'ai fait la collection users

---
+++ backend/models/users.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+const adressSchema = mongoose.Schema({
+    street: String,
+    city: String,
+    zipcode: String,
+})
+
+const hostSchema = mongoose.Schema({
+    hostRanking: Number,
+    description: String,
+    announces: [{ type: mongoose.Schema.Types.ObjectId, ref: 'announces' }],
+    favoriteGenre: [String],
+    hostedArtist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
+})
+
+const artistSchema = mongoose.Schema({
+    genre: [String],
+    member: Number,
+    artistName: String,
+    media: [String],
+    descripton: String,
+    placeOrigin: String,
+    tours: [{ type: mongoose.Schema.Types.ObjectId, ref: 'tours' }],
+    artistRanking: Number,
+})
+
+const userSchema = mongoose.Schema({
+    email: String,
+    username: String,
+    firstname: String,
+    lastname: String,
+    password: String,
+    token: String,
+    adress: adressSchema,
+    isArtist: Boolean,
+    isHost: Boolean,
+    artists: artistSchema,
+    hosts: hostSchema,
+    phoneNumber: String,
+    birthdate: Date,
+    createdAt: Date,
+    updatedAt: Date,
+    profilePicture: String,
+})
