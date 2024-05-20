e3dc67bf087687bccec64b86ac5ae46e5b8c9d0c
0xsterben <0xsterben@tutamail.com>
2024-05-20 17:37:45 +0200

    kjh

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,34 @@
 var express = require("express");
 var router = express.Router();
 import { checkBody } from "../modules/checkBody";
+const bcrypt = require("bcrypt")
+
+
+router.post("/signup", (req, res) => {
+    if (!checkBody(req.body, ['username', 'password', 'firstname'])) {
+        res.json({ result: false, error: 'Missing or empty fields' });
+        return;
+    }
+
+    User.findOne({ username: req.body.username }).then(data => {
+        if (data === null) {
+            const hash = bcrypt.hashSync(req.body.password, 10);
+            const newUser = new User({
+                firstname: req.body.firstname,
+                username: req.body.username,
+                password: hash,
+
+            });
+
+            newUser.save().then(newDoc => {
+                res.json({ result: true, token: newDoc.token });
+            });
+        } else {
+            // User already exists in database
+            res.json({ result: false, error: 'User already exists' });
+        }
+    });
+});
 
 router.post("/signin", (req, res) => {
     if (!checkBody(req.body, ["username", "password"])) {
