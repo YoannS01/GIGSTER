f4e0479867279bdf08a114b8333980c24a074e33
David.Kfld <david.s.kufeld@gmail.com>
2024-05-21 11:52:19 +0200

    Modif StatusScreen

--- frontend/screens/StatusScreen.jsx
+++ frontend/screens/StatusScreen.jsx
@@ -1,3 +1,18 @@
+import { TouchableOpacity, View } from "react-native";
+
 export default function StatusScreen() {
 
+
+
+    return (
+        <View>
+            <Text>Quel profil es tu ?</Text>
+            <TouchableOpacity>
+                <Text>Artiste</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>Hôte</Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
