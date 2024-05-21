3804d235dffcc658fc663cf563f2609fd5a61048
Julien BARRA <Julien.barra89100@gmail.coml>
2024-05-21 11:32:19 +0200

    login screen ok

--- frontend/App.js
+++ frontend/App.js
@@ -1,35 +1,37 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import HomeScreen from './screens/homeScreen';
-import LoginScreen from './screens/loginScreen';
-import ProfileScreen from './screens/profileScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import HomeScreen from "./screens/homeScreen";
+import LoginScreen from "./screens/loginScreen";
+import ProfileScreen from "./screens/profileScreen";
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={({ route }) => ({
-      tabBarIcon: ({ color, size }) => {
-        let iconName = '';
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName = "";
 
-        if (route.name === 'Home') {
-          iconName = 'location-arrow';
-        } else if (route.name === 'Profile') {
-          iconName = 'map-pin';
-        }
+          if (route.name === "Home") {
+            iconName = "location-arrow";
+          } else if (route.name === "Profile") {
+            iconName = "map-pin";
+          }
 
-        return <FontAwesome name={iconName} size={size} color={color} />;
-      },
-      tabBarActiveTintColor: '#ec6e5b',
-      tabBarInactiveTintColor: '#335561',
-      headerShown: false,
-    })}>
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#ec6e5b",
+        tabBarInactiveTintColor: "#335561",
+        headerShown: false,
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
@@ -51,8 +53,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- frontend/screens/loginScreen.jsx
+++ frontend/screens/loginScreen.jsx
@@ -1,125 +1,130 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
-import { useDispatch } from 'react-redux';
-import { updateEmail } from '../reducers/user';
+import { useState } from "react";
 
-const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const EMAIL_REGEX =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default function LoginScreen({ navigation }) {
+  //Vérification de la validité de l'email lors du Sign In
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
 
+  //Ci-dessous, le "screen: Home" est à créer/modifier
+  const handleSubmit = () => {
+    if (EMAIL_REGEX.test(email)) {
+      navigation.navigate("TabNavigator", { screen: "Home" });
+    } else {
+      setEmailError(true);
+    }
+  };
 
-    //Vérification de la validité de l'email lors du Sign In 
-    const dispatch = useDispatch();
-    const [email, setEmail] = useState('');
-    const [emailError, setEmailError] = useState(false);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.signin}>Sign In</Text>
+      <Text style={styles.titles}>Email</Text>
+      <TextInput
+        placeholder="name@example.com"
+        style={styles.input_email}
+        onChangeText={(value) => setEmail(value)}
+        value={email}
+      >
+        {emailError && <Text style={styles.error}>Invalid email address</Text>}
+      </TextInput>
+      <Text style={styles.titles}>Password</Text>
+      <TextInput
+        placeholder="Insert your password"
+        style={styles.input_password}
+      ></TextInput>
+      <TouchableOpacity style={styles.input_signin_button}>
+        <Text style={styles.text_signin} onPress={() => handleSubmit()}>
+          Sign In
+        </Text>
+      </TouchableOpacity>
 
-
-    //Ci-dessous, le "screen: Home" est à créer/modifier
-    const handleSubmit = () => {
-        if (EMAIL_REGEX.test(email)) {
-            dispatch(updateEmail(email));
-            navigation.navigate('TabNavigator', { screen: 'Home' });
-        } else {
-            setEmailError(true);
-        }
-    };
-
-
-    return (
-        <View style={styles.container}>
-            <Image source={require('./assets/daijobu.jpeg')} style={styles.image} />
-            <Text style={styles.signin}>Sign In</Text>
-            <Text style={styles.titles}>Email</Text>
-            <TextInput placeholder="name@example.com" style={styles.input_email} onChangeText={(value) => setEmail(value)}
-                value={email}>{emailError && <Text style={styles.error}>Invalid email address</Text>}</TextInput>
-            <Text style={styles.titles}>Password</Text>
-            <TextInput placeholder="Insert your password" style={styles.input_password}></TextInput>
-            <TouchableOpacity style={styles.input_signin_button}>
-                <Text style={styles.text_signin} onPress={() => handleSubmit()}>Sign In</Text>
-            </TouchableOpacity>
-
-            <Text>or use one of your social profiles</Text>
-            <View style={styles.bottom}>
-                <Text style={styles.bottom_text}>Pas encore inscrit ?</Text>
-                <Text style={styles.bottom_signup}>Sign Up</Text>
-            </View>
-
-        </View>
-    );
+      <Text>or use one of your social profiles</Text>
+      <View style={styles.bottom}>
+        <Text style={styles.bottom_text}>Pas encore inscrit ?</Text>
+        <Text style={styles.bottom_signup}>Sign Up</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#e1f5ff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    image: {
-        marginTop: 50,
-        marginBottom: 50,
-        height: '22%',
-        width: '50%',
-        borderRadius: 100,
-    },
-    signin: {
-        fontWeight: 700,
-        fontSize: 50,
-        marginTop: 10,
-    },
-    titles: {
-        fontWeight: '600',
-        fontSize: 20,
-        marginTop: 15,
-
-    },
-    input_email: {
-        height: '5%',
-        width: '80%',
-        backgroundColor: '#FFFFFF',
-        padding: 10,
-        marginTop: 5,
-    },
-    input_password: {
-        height: '5%',
-        width: '80%',
-        backgroundColor: '#FFFFFF',
-        padding: 10,
-        marginTop: 5,
-    },
-    text_signin: {
-
-        backgroundColor: '#ec2761',
-        color: '#FFFFFF',
-        fontWeight: '600',
-        fontSize: 16,
-        paddingBottom: 8,
-        paddingTop: 8,
-        paddingLeft: 130,
-        paddingRight: 130,
-        height: '100%',
-
-    },
-    input_signin_button: {
-        height: '6%',
-        width: '100%',
-        alignItems: 'center',
-        paddingTop: 10,
-        marginTop: 20,
-        marginBottom: 10,
-
-    },
-    bottom: {
-        alignItems: 'space-between',
-        paddingTop: 100,
-    },
-    bottom_text: {
-        marginRight: 200,
-    },
-    bottom_signup: {
-        color: '#ec2761',
-        fontWeight: '800',
-
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#e1f5ff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    marginTop: 50,
+    marginBottom: 50,
+    height: "22%",
+    width: "50%",
+    borderRadius: 100,
+  },
+  signin: {
+    fontWeight: 700,
+    fontSize: 50,
+    marginTop: 10,
+  },
+  titles: {
+    fontWeight: "600",
+    fontSize: 20,
+    marginTop: 15,
+  },
+  input_email: {
+    height: "5%",
+    width: "80%",
+    backgroundColor: "#FFFFFF",
+    padding: 10,
+    marginTop: 5,
+  },
+  input_password: {
+    height: "5%",
+    width: "80%",
+    backgroundColor: "#FFFFFF",
+    padding: 10,
+    marginTop: 5,
+  },
+  text_signin: {
+    backgroundColor: "#ec2761",
+    color: "#FFFFFF",
+    fontWeight: "600",
+    fontSize: 16,
+    paddingBottom: 8,
+    paddingTop: 8,
+    paddingLeft: 130,
+    paddingRight: 130,
+    height: "100%",
+  },
+  input_signin_button: {
+    height: "6%",
+    width: "100%",
+    alignItems: "center",
+    paddingTop: 10,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  bottom: {
+    alignItems: "space-between",
+    paddingTop: 100,
+  },
+  bottom_text: {
+    marginRight: 200,
+  },
+  bottom_signup: {
+    color: "#ec2761",
+    fontWeight: "800",
+  },
 });
