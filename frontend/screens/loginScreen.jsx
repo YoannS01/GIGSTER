a6a03bd4bb184e0d78014a11bfa282160042cd2e
Julien BARRA <Julien.barra89100@gmail.coml>
2024-05-21 13:59:41 +0200

    signIn and signup frontend mecanism ok / missing fetch btw

--- frontend/screens/loginScreen.jsx
+++ frontend/screens/loginScreen.jsx
@@ -12,40 +12,77 @@ import { useState } from "react";
 
 const EMAIL_REGEX =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const frontIp = process.env.FRONT_IP;
 
 export default function LoginScreen({ navigation }) {
-  //Vérification de la validité de l'email lors du Sign In
-  const [email, setEmail] = useState("");
+  //Variables d'états
+  const [emailSignIn, setEmailSignIn] = useState("");
+  const [emailSignUp, setEmailSignUp] = useState("");
+  const [passwordSignIn, setPasswordSignIn] = useState("");
+  const [passwordSignUp, setPasswordSignUp] = useState("");
+  const [confirmPasswordSignUp, setConfirmPasswordSignUp] = useState("");
+  const [username, setUsername] = useState("");
+  const [isSignIn, setIsSignIn] = useState(false);
   const [emailError, setEmailError] = useState(false);
 
-  //Ci-dessous, le "screen: Home" est à créer/modifier
-  const handleSubmit = () => {
+  const handleSubmitSignup = () => {
     if (EMAIL_REGEX.test(email)) {
-      navigation.navigate("TabNavigator", { screen: "Home" });
+      fetch(`http://${frontIp}:3000/users/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          navigation.navigate("TabNavigator", { screen: "Home" });
+        });
     } else {
       setEmail("");
       setEmailError(true);
     }
   };
 
-  return (
+  const handleSubmitSignIn = () => {
+    //Vérification de la validité de l'email lors du Sign In
+    if (EMAIL_REGEX.test(email)) {
+      fetch(`http://${frontIp}:3000/users/signin`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          navigation.navigate("TabNavigator", { screen: "Home" });
+        });
+    } else {
+      setEmailSignIn("");
+      setEmailError(true);
+    }
+  };
+
+  return !isSignIn ? (
+    //SIGN IN SCREEN :
     <View style={styles.container}>
       <Text style={styles.signin}>Sign In</Text>
       <Text style={styles.titles}>Email</Text>
       <TextInput
         placeholder="name@example.com"
         style={styles.input_email}
-        onChangeText={(value) => setEmail(value)}
-        value={email}
+        onChangeText={(value) => setEmailSignIn(value)}
+        value={emailSignIn}
       ></TextInput>
       {emailError && <Text style={styles.error}>Invalid email address</Text>}
       <Text style={styles.titles}>Password</Text>
       <TextInput
         placeholder="Insert your password"
         style={styles.input_password}
+        onChangeText={(value) => setPasswordSignIn(value)}
+        value={passwordSignIn}
       ></TextInput>
       <TouchableOpacity style={styles.input_signin_button}>
-        <Text style={styles.text_signin} onPress={() => handleSubmit()}>
+        <Text style={styles.text_signin} onPress={() => handleSubmitSignIn()}>
           Sign In
         </Text>
       </TouchableOpacity>
@@ -53,8 +90,49 @@ export default function LoginScreen({ navigation }) {
       <Text>or use one of your social profiles</Text>
       <View style={styles.bottom}>
         <Text style={styles.bottom_text}>Pas encore inscrit ?</Text>
-        <Text style={styles.bottom_signup}>Sign Up</Text>
+        <Text style={styles.bottom_signup} onPress={() => setIsSignIn(true)}>
+          Sign Up
+        </Text>
       </View>
+    </View>
+  ) : (
+    //SIGN UP SCREEN :
+    <View style={styles.container}>
+      <Text style={styles.signup}>Sign Up</Text>
+      <Text style={styles.titles}>Username</Text>
+      <TextInput
+        placeholder="Username"
+        style={styles.input_username}
+        onChangeText={(value) => setUsername(value)}
+        value={username}
+      ></TextInput>
+      <Text style={styles.titles}>Email</Text>
+      <TextInput
+        placeholder="name@example.com"
+        style={styles.input_email}
+        onChangeText={(value) => setEmailSignUp(value)}
+        value={emailSignUp}
+      ></TextInput>
+      {emailError && <Text style={styles.error}>Invalid email address</Text>}
+      <Text style={styles.titles}>Password</Text>
+      <TextInput
+        placeholder="Insert your password"
+        style={styles.input_password}
+        onChangeText={(value) => setPasswordSignUp(value)}
+        value={passwordSignUp}
+      ></TextInput>
+      <Text style={styles.titles}>Confirm your Password</Text>
+      <TextInput
+        placeholder="Confirm your password"
+        style={styles.input_password}
+        onChangeText={(value) => setConfirmPasswordSignUp(value)}
+        value={confirmPasswordSignUp}
+      ></TextInput>
+      <TouchableOpacity style={styles.input_signup_button}>
+        <Text style={styles.text_signup} onPress={() => handleSubmitSignup()}>
+          Sign Up
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
