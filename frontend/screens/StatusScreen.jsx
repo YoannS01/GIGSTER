4de68c9ffc6478b1eb7b911a2b500c18250b6327
YoannS01 <yoasaget@gmail.com>
2024-05-21 17:50:10 +0200

    commit

--- frontend/screens/StatusScreen.jsx
+++ frontend/screens/StatusScreen.jsx
@@ -1,18 +1,53 @@
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View, Text } from "react-native";
 
 export default function StatusScreen() {
 
 
 
     return (
-        <View>
-            <Text>Quel profil es tu ?</Text>
-            <TouchableOpacity>
-                <Text>Artiste</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Je suis un..</Text>
+            <TouchableOpacity style={styles.input_button}>
+                <Text style={styles.text_button}>Artiste</Text>
             </TouchableOpacity>
-            <TouchableOpacity>
-                <Text>Hôte</Text>
+            <TouchableOpacity style={styles.input_button}>
+                <Text style={styles.text_button}>Hôte</Text>
             </TouchableOpacity>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#e1f5ff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 20,
+        marginTop: 15,
+    },
+    input_button: {
+        height: "6%",
+        width: "100%",
+        alignItems: "center",
+        paddingTop: 10,
+        marginTop: 20,
+        marginBottom: 10,
+    },
+    text_button: {
+        backgroundColor: "#ec2761",
+        color: "#FFFFFF",
+        fontWeight: "bold",
+        fontSize: 16,
+        paddingBottom: 8,
+        paddingTop: 8,
+        paddingLeft: 130,
+        paddingRight: 130,
+        height: "100%",
+    },
+
+})
