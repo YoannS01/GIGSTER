e3cd073d4fba4fe2ed2725c108f00d3a3117c502
Julien BARRA <Julien.barra89100@gmail.coml>
2024-05-23 11:43:15 +0200

    jijij

--- frontend/screens/loginScreen.jsx
+++ frontend/screens/loginScreen.jsx
@@ -1,11 +1,13 @@
 import { FRONT_IP } from "../hide-ip";
-import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
   Text,
   TextInput,
   View,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
 } from "react-native";
 
 import { useState } from "react";
@@ -19,7 +21,7 @@ import {
 } from "../reducers/user";
 
 const EMAIL_REGEX =
-  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
 
 export default function LoginScreen({ navigation }) {
   //Variables d'états
@@ -52,6 +54,10 @@ export default function LoginScreen({ navigation }) {
         .then((data) => {
           if (!data.result) {
             setError(true);
+            setEmailSignUp("");
+            setPasswordSignUp("");
+            setConfirmPasswordSignUp("");
+            setUsername("");
             setMessageError(data.error);
           } else {
             dispatch(updateUsername(username));
@@ -89,6 +95,8 @@ export default function LoginScreen({ navigation }) {
           console.log(data);
           if (!data.result) {
             setError(true);
+            setEmailSignIn("");
+            setPasswordSignIn("");
             setMessageError(data.error);
           } else {
             dispatch(updateUsername(data.data.username));
@@ -103,91 +111,102 @@ export default function LoginScreen({ navigation }) {
       setEmailSignIn("");
       setPasswordSignIn("");
       setError(true);
-      setMessageError("Missing or empty fields");
+      setMessageError("Invalid email adress");
     }
   };
 
   return !isSignIn ? (
-    //SIGN IN SCREEN :
-    <View style={styles.container}>
-      <Text style={styles.signin}>Sign In</Text>
-      <Text style={styles.titles}>Email</Text>
-      <TextInput
-        placeholder="name@example.com"
-        style={styles.input_email}
-        onChangeText={(value) => setEmailSignIn(value)}
-        value={emailSignIn}
-      ></TextInput>
-      <Text style={styles.titles}>Password</Text>
-      <TextInput
-        secureTextEntry={true}
-        placeholder="Insert your password"
-        style={styles.input_password}
-        onChangeText={(value) => setPasswordSignIn(value)}
-        value={passwordSignIn}
-      ></TextInput>
-      {Error && <Text style={styles.error}>{messageError}</Text>}
-      <TouchableOpacity style={styles.input_signin_button}>
-        <Text style={styles.text_signin} onPress={() => handleSubmitSignIn()}>
-          Sign In
-        </Text>
-      </TouchableOpacity>
-
-      <Text>or use one of your social profiles</Text>
-      <View style={styles.bottom}>
-        <Text style={styles.bottom_text}>Pas encore inscrit ?</Text>
-        <Text style={styles.bottom_signup} onPress={() => setIsSignIn(true)}>
-          Sign Up
-        </Text>
-      </View>
-    </View>
+    // SIGN IN SCREEN :
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 10 : 0}
+    >
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+        <Text style={styles.signin}>Sign In</Text>
+        <Text style={styles.titles}>Email</Text>
+        <TextInput
+          placeholder="name@example.com"
+          style={styles.input_email}
+          onChangeText={(value) => setEmailSignIn(value)}
+          value={emailSignIn}
+        />
+        <Text style={styles.titles}>Password</Text>
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Insert your password"
+          style={styles.input_password}
+          onChangeText={(value) => setPasswordSignIn(value)}
+          value={passwordSignIn}
+        />
+        {Error && <Text style={styles.error}>{messageError}</Text>}
+        <TouchableOpacity style={styles.input_signin_button}>
+          <Text style={styles.text_signin} onPress={() => handleSubmitSignIn()}>
+            Sign In
+          </Text>
+        </TouchableOpacity>
+        <Text>or use one of your social profiles</Text>
+        <View style={styles.bottom}>
+          <Text style={styles.bottom_text}>Pas encore inscrit ?</Text>
+          <Text style={styles.bottom_signup} onPress={() => setIsSignIn(true)}>
+            Sign Up
+          </Text>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   ) : (
-    //SIGN UP SCREEN :
-    <View style={styles.container}>
-      <Text style={styles.signup}>Sign Up</Text>
-      <Text style={styles.titles}>Username</Text>
-      <TextInput
-        placeholder="Username"
-        style={styles.input_username}
-        onChangeText={(value) => setUsername(value)}
-        value={username}
-      ></TextInput>
-      <Text style={styles.titles}>Email</Text>
-      <TextInput
-        placeholder="name@example.com"
-        style={styles.input_email}
-        onChangeText={(value) => setEmailSignUp(value)}
-        value={emailSignUp}
-      ></TextInput>
-      <Text style={styles.titles}>Password</Text>
-      <TextInput
-        secureTextEntry={true}
-        placeholder="Insert your password"
-        style={styles.input_password}
-        onChangeText={(value) => setPasswordSignUp(value)}
-        value={passwordSignUp}
-      ></TextInput>
-      <Text style={styles.titles}>Confirm your Password</Text>
-      <TextInput
-        secureTextEntry={true}
-        placeholder="Confirm your password"
-        style={styles.input_password}
-        onChangeText={(value) => setConfirmPasswordSignUp(value)}
-        value={confirmPasswordSignUp}
-      ></TextInput>
-      {Error && <Text style={styles.error}>{messageError}</Text>}
-      <TouchableOpacity style={styles.input_signup_button}>
-        <Text style={styles.text_signup} onPress={() => handleSubmitSignUp()}>
-          Sign Up
-        </Text>
-      </TouchableOpacity>
-      <View style={styles.bottom}>
-        <Text style={styles.bottom_text}>Déjà inscrit ?</Text>
-        <Text style={styles.bottom_signup} onPress={() => setIsSignIn(false)}>
-          Sign In
-        </Text>
-      </View>
-    </View>
+    // SIGN UP SCREEN :
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 10 : 0}
+    >
+      <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+        <Text style={styles.signup}>Sign Up</Text>
+        <Text style={styles.titles}>Username</Text>
+        <TextInput
+          placeholder="Username"
+          style={styles.input_username}
+          onChangeText={(value) => setUsername(value)}
+          value={username}
+        />
+        <Text style={styles.titles}>Email</Text>
+        <TextInput
+          placeholder="name@example.com"
+          style={styles.input_email}
+          onChangeText={(value) => setEmailSignUp(value)}
+          value={emailSignUp}
+        />
+        <Text style={styles.titles}>Password</Text>
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Insert your password"
+          style={styles.input_password}
+          onChangeText={(value) => setPasswordSignUp(value)}
+          value={passwordSignUp}
+        />
+        <Text style={styles.titles}>Confirm your Password</Text>
+        <TextInput
+          secureTextEntry={true}
+          placeholder="Confirm your password"
+          style={styles.input_password}
+          onChangeText={(value) => setConfirmPasswordSignUp(value)}
+          value={confirmPasswordSignUp}
+        />
+        {Error && <Text style={styles.error}>{messageError}</Text>}
+        <TouchableOpacity style={styles.input_signup_button}>
+          <Text style={styles.text_signup} onPress={() => handleSubmitSignUp()}>
+            Sign Up
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.bottom}>
+          <Text style={styles.bottom_text}>Déjà inscrit ?</Text>
+          <Text style={styles.bottom_signup} onPress={() => setIsSignIn(false)}>
+            Sign In
+          </Text>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -198,12 +217,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  scrollViewContainer: {
+    flexGrow: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   image: {
     marginTop: 50,
     marginBottom: 50,
     height: "22%",
     width: "50%",
     borderRadius: 100,
+  },
+  signup: {
+    fontWeight: "bold",
+    fontSize: 50,
+    marginTop: 10,
   },
   signin: {
     fontWeight: "bold",
@@ -215,16 +244,23 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginTop: 15,
   },
+  input_username: {
+    height: 40,
+    width: 300,
+    backgroundColor: "#FFFFFF",
+    padding: 10,
+    marginTop: 5,
+  },
   input_email: {
-    height: "5%",
-    width: "80%",
+    height: 40,
+    width: 300,
     backgroundColor: "#FFFFFF",
     padding: 10,
     marginTop: 5,
   },
   input_password: {
-    height: "5%",
-    width: "80%",
+    height: 40,
+    width: 300,
     backgroundColor: "#FFFFFF",
     padding: 10,
     marginTop: 5,
@@ -240,10 +276,31 @@ const styles = StyleSheet.create({
     paddingRight: 130,
     height: "100%",
   },
+  text_signup: {
+    backgroundColor: "#ec2761",
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 16,
+    paddingBottom: 8,
+    paddingTop: 8,
+    paddingLeft: 130,
+    paddingRight: 130,
+    height: "100%",
+  },
   input_signin_button: {
-    height: "6%",
-    width: "100%",
+    height: 50,
+    width: 350,
     alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 10,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  input_signup_button: {
+    height: 50,
+    width: 350,
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: 10,
     marginTop: 20,
     marginBottom: 10,
