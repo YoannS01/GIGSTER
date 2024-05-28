d7e770a2c4421e910a24f37b8851b439f04a29d4
David.Kfld <david.s.kufeld@gmail.com>
2024-05-28 17:18:59 +0200

    modif

--- frontend/components/Login.js
+++ frontend/components/Login.js
@@ -21,6 +21,7 @@ import { Formik } from "formik";
 import * as Yup from "yup";
 import { FRONT_IP } from "../hide-ip";
 import { useNavigation } from "@react-navigation/native";
+import moment from "moment";
 
 // Définir les schémas de validation avec Yup
 
@@ -46,7 +47,7 @@ const signUpSchema = Yup.object().shape({
         .required("Confirm Password is required"),
 });
 
-export default function Login(props) {
+export default function Login(props, navigate) {
     const navigation = useNavigation()
     const dispatch = useDispatch();
     const [isSignIn, setIsSignIn] = useState(false);
@@ -62,19 +63,29 @@ export default function Login(props) {
         })
             .then((response) => response.json())
             .then((data) => {
-                console.log("REPONSE DU BACK", data.result)
+                console.log("REPONSE DU BACK", data.data)
                 setSubmitting(false);
                 if (!data.result) {
                     setErrors({ general: data.error });
                 } else {
-                    //navigation est undefined
+                    delete data.data.artist._id;
+
+                    dispatch(updateToken(data.data.token));
+                    dispatch(updateUsername(data.data.username));
+                    dispatch(updateEmail(data.email));
+                    dispatch(updateFirstname(data.data.firstname));
+                    dispatch(updateLastname(data.data.lastname));
+                    dispatch(updateAddress(data.data.address));
+                    dispatch(updatePhoneNumber(data.data.phoneNumber));
+                    dispatch(getArtistInfos(data.data.artist));
+                    dispatch(getHostInfos(data.data.host));
+                    dispatch(updateBirthdate(moment(data.data.birthdate).format("DD/MM/YYYY")));
+                    dispatch(updateArtist(data.data.isArtist));
+                    dispatch(updateHost(data.data.isHost));
+
                     navigation.navigate("TabNavigator", { screen: "Home" });
-                    /* dispatch(updateUsername(data.data.username));
-                     dispatch(updateEmail(data.data.email));
-                     dispatch(updateToken(data.data.token));
-                     dispatch(updateArtist(data.data.isArtist));
-                     dispatch(updateHost(data.data.isHost));
- */
+
+
                 }
             })
 
