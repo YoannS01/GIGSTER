becb6e51a7d27c33cb284d623f0af768bf33fb7e
Julien BARRA <Julien.barra89100@gmail.coml>
2024-05-28 16:29:52 +0200

    me

--- frontend/screens/addScreen.jsx
+++ frontend/screens/addScreen.jsx
@@ -4,6 +4,8 @@ import diyTourScreen from "./diyTourScreen";
 import announceScreen from "./announceScreen";
 
 export default function AddScreen() {
+  const user = useSelector((state) => state.user.value);
+  console.log("REDUCER ==>", user);
   const userIsArtist = useSelector((state) => state.user.value.isArtist);
   const userIsHost = useSelector((state) => state.user.value.isHost);
 
