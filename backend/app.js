e897d096c1a17b06eba42d8a23815aea6c4187f7
0xsterben <0xsterben@tutamail.com>
2024-05-29 10:23:37 +0200

    J'ai changé le nom des fichiers

--- backend/app.js
+++ backend/app.js
@@ -3,25 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fileUpload = require('express-fileupload');
+const cors = require('cors');
 
-require('./models/connection')
+require('./models/connection');
 const auth = require('./middleware/auth');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-//var authUsersRouter = require('./routes/authUsers')
+var authUsersRouter = require('./routes/authUsers');
 
 var app = express();
-const cors = require('cors');
-app.use(cors());
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//app.use('/authUsers', auth, authUsersRouter)
+app.use('/authUsers', auth, authUsersRouter);
 
 module.exports = app;
