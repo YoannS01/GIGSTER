e9ffc4e3e521b591abd29cbf03d63d201cd87d08
Julien BARRA <Julien.barra89100@gmail.coml>
2024-05-29 18:35:20 +0200

    fix signup Host bug

--- frontend/components/Step2.js
+++ frontend/components/Step2.js
@@ -1,222 +1,226 @@
 import React from "react";
 import {
-    Alert,
-    StyleSheet,
-    TextInput,
-    TouchableOpacity,
-    View,
-    Text,
-    KeyboardAvoidingView,
-    Platform,
+  Alert,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { useFormik } from "formik";
 import * as yup from "yup";
 
 export default function Step2(props) {
-    // Validation schema for artist
-    const artistSchema = yup.object().shape({
-        artistname: yup.string().required("Artist name is required"),
-        members: yup
-            .number()
-            .required("Members is required")
-            .min(1, "Must be at least 1 member"),
-        genres: yup.string().required("Genre is required"),
-        placeOrigin: yup.string().required("Place of origin is required"),
-    });
+  // Validation schema for artist
+  const artistSchema = yup.object().shape({
+    artistname: yup.string().required("Artist name is required"),
+    members: yup
+      .number()
+      .required("Members is required")
+      .min(1, "Must be at least 1 member"),
+    genres: yup.string().required("Genre is required"),
+    placeOrigin: yup.string().required("Place of origin is required"),
+  });
 
-    // Validation schema for host
-    const hostSchema = yup.object().shape({
-        description: yup.string().required("Description is required"),
-        favoritesGenres: yup.string().required("Favorite genre is required"),
-    });
+  // Validation schema for host
+  const hostSchema = yup.object().shape({
+    description: yup.string().required("Description is required"),
+    favoritesGenres: yup.string().required("Favorite genre is required"),
+  });
 
-    const initialValues = props.user.isArtist
-        ? { artistname: "", members: "", genres: "", placeOrigin: "" }
-        : { description: "", favoritesGenres: "" };
+  const initialValues = props.user.isArtist
+    ? { artistname: "", members: "", genres: "", placeOrigin: "" }
+    : { description: "", favoritesGenres: "" };
 
-    const validationSchema = props.user.isArtist ? artistSchema : hostSchema;
+  const validationSchema = props.user.isArtist ? artistSchema : hostSchema;
 
-    const formik = useFormik({
-        initialValues,
-        validationSchema,
-        onSubmit: (values) => {
-            if (props.user.isArtist) {
-                props.updateUser({
-                    artist: {
-                        artistname: values.artistname,
-                        members: parseInt(values.members, 10),
-                        placeOrigin: values.placeOrigin,
-                        genres: values.genres.split(","),
-                    },
-                });
-            } else if (props.user.isHost) {
-                props.updateUser({
-                    host: {
-                        description: values.description,
-                        favoritesGenres: values.favoritesGenres.split(","),
-                    },
-                });
-            }
-            props.getNextPage(5);
-        },
-    });
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      console.log("IS HOST===> ", props.user.isHost);
+      if (props.user.isArtist) {
+        props.updateUser({
+          artist: {
+            artistname: values.artistname,
+            members: parseInt(values.members, 10),
+            placeOrigin: values.placeOrigin,
+            genres: values.genres.split(","),
+          },
+        });
+      } else if (props.user.isHost) {
+        props.updateUser({
+          host: {
+            description: values.description,
+            favoritesGenres: values.favoritesGenres.split(","),
+          },
+        });
+      }
+      props.getNextPage(5);
+    },
+  });
 
-    const previousPage = () => {
-        props.getNextPage(3);
-    };
+  const previousPage = () => {
+    props.getNextPage(3);
+  };
 
-    return (
-        <KeyboardAvoidingView
-            style={styles.container}
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            keyboardVerticalOffset={Platform.OS === "ios" ? 10 : 0}
-        >
-            <View>
-                <Text>Mon Profil</Text>
-                <View style={styles.stepContent}>
-                    {props.user.isArtist ? (
-                        <>
-                            <Text style={styles.label}>Artist name</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="name"
-                                onChangeText={formik.handleChange("artistname")}
-                                onBlur={formik.handleBlur("artistname")}
-                                value={formik.values.artistname}
-                            />
-                            {formik.touched.artistname && formik.errors.artistname ? (
-                                <Text style={styles.error}>{formik.errors.artistname}</Text>
-                            ) : null}
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 10 : 0}
+    >
+      <View>
+        <Text>Mon Profil</Text>
+        <View style={styles.stepContent}>
+          {props.user.isArtist ? (
+            <>
+              <Text style={styles.label}>Artist name</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="name"
+                onChangeText={formik.handleChange("artistname")}
+                onBlur={formik.handleBlur("artistname")}
+                value={formik.values.artistname}
+              />
+              {formik.touched.artistname && formik.errors.artistname ? (
+                <Text style={styles.error}>{formik.errors.artistname}</Text>
+              ) : null}
 
-                            <Text style={styles.label}>Members</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Number"
-                                keyboardType="numeric"
-                                onChangeText={formik.handleChange("members")}
-                                onBlur={formik.handleBlur("members")}
-                                value={formik.values.members}
-                            />
-                            {formik.touched.members && formik.errors.members ? (
-                                <Text style={styles.error}>{formik.errors.members}</Text>
-                            ) : null}
+              <Text style={styles.label}>Members</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Number"
+                keyboardType="numeric"
+                onChangeText={formik.handleChange("members")}
+                onBlur={formik.handleBlur("members")}
+                value={formik.values.members}
+              />
+              {formik.touched.members && formik.errors.members ? (
+                <Text style={styles.error}>{formik.errors.members}</Text>
+              ) : null}
 
-                            <Text style={styles.label}>Genre</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Genre"
-                                onChangeText={formik.handleChange("genres")}
-                                onBlur={formik.handleBlur("genres")}
-                                value={formik.values.genres}
-                            />
-                            {formik.touched.genres && formik.errors.genres ? (
-                                <Text style={styles.error}>{formik.errors.genres}</Text>
-                            ) : null}
+              <Text style={styles.label}>Genre</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Genre"
+                onChangeText={formik.handleChange("genres")}
+                onBlur={formik.handleBlur("genres")}
+                value={formik.values.genres}
+              />
+              {formik.touched.genres && formik.errors.genres ? (
+                <Text style={styles.error}>{formik.errors.genres}</Text>
+              ) : null}
 
-                            <Text style={styles.label}>Place of origin</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="City"
-                                onChangeText={formik.handleChange("placeOrigin")}
-                                onBlur={formik.handleBlur("placeOrigin")}
-                                value={formik.values.placeOrigin}
-                            />
-                            {formik.touched.placeOrigin && formik.errors.placeOrigin ? (
-                                <Text style={styles.error}>{formik.errors.placeOrigin}</Text>
-                            ) : null}
-                        </>
-                    ) : (
-                        <>
-                            <Text style={styles.label}>Description</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Description"
-                                onChangeText={formik.handleChange("description")}
-                                onBlur={formik.handleBlur("description")}
-                                value={formik.values.description}
-                            />
-                            {formik.touched.description && formik.errors.description ? (
-                                <Text style={styles.error}>{formik.errors.description}</Text>
-                            ) : null}
+              <Text style={styles.label}>Place of origin</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="City"
+                onChangeText={formik.handleChange("placeOrigin")}
+                onBlur={formik.handleBlur("placeOrigin")}
+                value={formik.values.placeOrigin}
+              />
+              {formik.touched.placeOrigin && formik.errors.placeOrigin ? (
+                <Text style={styles.error}>{formik.errors.placeOrigin}</Text>
+              ) : null}
+            </>
+          ) : (
+            <>
+              <Text style={styles.label}>Description</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Description"
+                onChangeText={formik.handleChange("description")}
+                onBlur={formik.handleBlur("description")}
+                value={formik.values.description}
+              />
+              {formik.touched.description && formik.errors.description ? (
+                <Text style={styles.error}>{formik.errors.description}</Text>
+              ) : null}
 
-                            <Text style={styles.label}>Favorite genre(s)</Text>
-                            <TextInput
-                                style={styles.input}
-                                placeholder="Favorite genre"
-                                onChangeText={formik.handleChange("favoriteGenre")}
-                                onBlur={formik.handleBlur("favoriteGenre")}
-                                value={formik.values.favoriteGenre}
-                            />
-                            {formik.touched.favoriteGenre && formik.errors.favoriteGenre ? (
-                                <Text style={styles.error}>{formik.errors.favoriteGenre}</Text>
-                            ) : null}
-                        </>
-                    )}
-                </View>
+              <Text style={styles.label}>Favorite genre(s)</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Favorite genre"
+                onChangeText={formik.handleChange("favoritesGenres")}
+                onBlur={formik.handleBlur("favoritesGenres")}
+                value={formik.values.favoritesGenres}
+              />
+              {formik.touched.favoritesGenres &&
+              formik.errors.favoritesGenres ? (
+                <Text style={styles.error}>
+                  {formik.errors.favoritesGenres}
+                </Text>
+              ) : null}
+            </>
+          )}
+        </View>
 
-                <TouchableOpacity style={styles.btn} onPress={formik.handleSubmit}>
-                    <Text style={styles.textBtn}>Submit</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.btn} onPress={previousPage}>
-                    <Text style={styles.textBtn}>Previous</Text>
-                </TouchableOpacity>
-            </View>
-        </KeyboardAvoidingView>
-    );
+        <TouchableOpacity style={styles.btn} onPress={formik.handleSubmit}>
+          <Text style={styles.textBtn}>Submit</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={previousPage}>
+          <Text style={styles.textBtn}>Previous</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
 
 const styles = StyleSheet.create({
-    mainSelect: {
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        backgroundColor: "white",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    label: {
-        fontSize: 16,
-        marginHorizontal: 5,
-        marginTop: 10,
-    },
-    input: {
-        backgroundColor: "white",
-        paddingTop: 6,
-        paddingBottom: 6,
-        paddingLeft: 10,
-        paddingRight: 10,
-        borderRadius: 8,
-        borderWidth: 1,
-        borderBottomWidth: 3,
-        borderRightWidth: 3,
-        marginTop: 5,
-    },
-    btn: {
-        backgroundColor: "#5100FF",
-        paddingTop: 6,
-        paddingBottom: 6,
-        paddingLeft: 10,
-        paddingRight: 10,
-        borderRadius: 8,
-        borderWidth: 1,
-        borderBottomWidth: 3,
-        borderRightWidth: 3,
-        marginTop: 10,
-    },
-    stepContent: {
-        marginVertical: 20,
-    },
-    textBtn: {
-        color: "white",
-        textAlign: "center",
-        fontSize: 16,
-    },
-    error: {
-        color: "red",
-        fontSize: 12,
-        marginHorizontal: 5,
-        marginTop: 5,
-    },
+  mainSelect: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  label: {
+    fontSize: 16,
+    marginHorizontal: 5,
+    marginTop: 10,
+  },
+  input: {
+    backgroundColor: "white",
+    paddingTop: 6,
+    paddingBottom: 6,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderBottomWidth: 3,
+    borderRightWidth: 3,
+    marginTop: 5,
+  },
+  btn: {
+    backgroundColor: "#5100FF",
+    paddingTop: 6,
+    paddingBottom: 6,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderBottomWidth: 3,
+    borderRightWidth: 3,
+    marginTop: 10,
+  },
+  stepContent: {
+    marginVertical: 20,
+  },
+  textBtn: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 16,
+  },
+  error: {
+    color: "red",
+    fontSize: 12,
+    marginHorizontal: 5,
+    marginTop: 5,
+  },
 });
 
 /*import { useState } from "react";
