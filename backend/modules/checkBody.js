7162cadaca271310502c925a8d1789609248c5bb
Julien BARRA <Julien.barra89100@gmail.coml>
2024-05-30 17:51:50 +0200

    create announce ok

--- backend/modules/checkBody.js
+++ backend/modules/checkBody.js
@@ -2,7 +2,8 @@ function checkBody(body, keys) {
   let isValid = true;
 
   for (const field of keys) {
-    if (!body[field] || body[field] === '') {
+    if (!body[field] || body[field] === "") {
+      console.log("FIELD MANQUANTS ===> ", field);
       isValid = false;
     }
   }
