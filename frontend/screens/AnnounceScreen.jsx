6aa332a6836b4aac3606f8e52565f0ceaab5c828
0xsterben <0xsterben@tutamail.com>
2024-05-30 15:58:24 +0200

    corection de l'agencement des fichiers

--- frontend/screens/AnnounceScreen.jsx
+++ frontend/screens/AnnounceScreen.jsx
@@ -202,7 +202,7 @@ export default function AnnounceScreen() {
 
   // fetch vers la DB pour envoyer les données des inputs
   const handleSubmit = () => {
-    fetch(`http://${FRONT_IP}:3000/authUsers/announces`, {
+    fetch(`http://${FRONT_IP}:3000/announces`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
