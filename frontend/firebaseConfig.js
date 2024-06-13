3324ed7afde7ed9cc3ea6951b339c92d0b9e354f
Julien BARRA <Julien.barra89100@gmail.coml>
2024-06-13 16:59:28 +0200

    jj

--- frontend/firebaseConfig.js
+++ frontend/firebaseConfig.js
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
 // Votre configuration Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDPJ3Ki5yOZAm-gfZHV5tZJARNHYQpueWU",
-    authDomain: "gigster-b98b9.firebaseapp.com",
-    projectId: "gigster-b98b9",
-    storageBucket: "gigster-b98b9.appspot.com",
-    messagingSenderId: "242284250606",
-    appId: "1:242284250606:web:04912309c73deb72fcfaff",
-    measurementId: "G-DPDCHVGJ2B"
+  apiKey: "AIzaSyDPJ3Ki5yOZAm-gfZHV5tZJARNHYQpueWU",
+  authDomain: "gigster-b98b9.firebaseapp.com",
+  projectId: "gigster-b98b9",
+  storageBucket: "gigster-b98b9.appspot.com",
+  messagingSenderId: "242284250606",
+  appId: "1:242284250606:web:04912309c73deb72fcfaff",
+  measurementId: "G-DPDCHVGJ2B",
 };
 
 // Initialiser Firebase
